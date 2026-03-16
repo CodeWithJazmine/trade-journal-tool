9ed3b16c215fa0ffe3f3fa30d7feafc735c7672f
CodeWithJazmine <71017555+CodeWithJazmine@users.noreply.github.com>
2026-03-15 18:12:08 -0700

    fix middleware for vercel

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,5 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server'
 
 const isPublicRoute = createRouteMatcher([
   '/',
@@ -10,6 +11,7 @@ export default clerkMiddleware(async (auth, request) => {
   if (!isPublicRoute(request)) {
     await auth.protect()
   }
+  return NextResponse.next()
 })
 
 export const config = {
