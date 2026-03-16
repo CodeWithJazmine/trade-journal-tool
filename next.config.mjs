0d41ca51554fc1296a012b6d8248a70aa9767988
CodeWithJazmine <71017555+CodeWithJazmine@users.noreply.github.com>
2026-03-15 18:34:44 -0700

    switch to webpack, successful build

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  serverExternalPackages: ['@clerk/nextjs', '@clerk/backend', '@clerk/shared'],
-  bundler: 'webpack',
-}
+const nextConfig = {}
 
 export default nextConfig
